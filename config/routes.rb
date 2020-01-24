df186198fcda3d26dbbb86550b6f763d0c404d80
Sasha <apestruy@gmail.com>
2020-01-24 11:04:55 -0500

    basic deliverables

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,30 @@
 Rails.application.routes.draw do
-  resources :powers, only: [:index]
-  resources :heroines, only: [:index]  
-end
+
+#Heroine Routes
+
+get "/heroines", to: "heroines#index", as: "heroines"
+get "/heroines/new", to: "heroines#new", as: "new_heroine"
+get "/heroines/:id", to: "heroines#show", as: "heroine"
+post "/heroines", to: "heroines#create"
+
+
+#Powers Routes
+get "/powers", to: "powers#index", as: "powers"
+get "/powers/:id", to: "powers#show", as: "power"
+
+
+end 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
